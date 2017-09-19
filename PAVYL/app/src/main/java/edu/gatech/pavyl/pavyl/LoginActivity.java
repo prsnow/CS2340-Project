bd9608c968e271f5aac157ecb812284c69eedade
aidancbrady <me@aidancbrady.com>
2017-09-18 21:45:51 -0400

    Built UI

--- PAVYL/app/src/main/java/edu/gatech/pavyl/pavyl/LoginActivity.java
+++ PAVYL/app/src/main/java/edu/gatech/pavyl/pavyl/LoginActivity.java
@@ -3,11 +3,13 @@ package edu.gatech.pavyl.pavyl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WelcomeActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_login);
     }
 }
--- PAVYL/app/src/main/java/edu/gatech/pavyl/pavyl/MainActivity.java
+++ PAVYL/app/src/main/java/edu/gatech/pavyl/pavyl/MainActivity.java
@@ -3,11 +3,11 @@ package edu.gatech.pavyl.pavyl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
     }
 }
